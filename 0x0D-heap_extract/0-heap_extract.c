3c639c80629649d04bae6d96962a215e67f8dcca
meghamohan <meghamohan1989@gmail.com>
2018-09-09 20:30:24 +0000

    second commit; gives output

--- 0x0D-heap_extract/0-heap_extract.c
+++ 0x0D-heap_extract/0-heap_extract.c
@@ -2,6 +2,34 @@
 
 
 /**
+ * reorder - restructure the heap
+ * @heap: pointer to the heapnode
+ */
+void reorder(heap_t *heap)
+{
+    heap_t *num;
+    int temp;
+
+    if (heap == NULL)
+        return;
+    if (heap->left && heap->left->n > heap->n)
+        num = heap->left;
+    else
+        num = heap;
+    if (heap->right && heap->right->n > num->n)
+        num = heap->right;
+
+    if (num != heap)
+    {
+        temp = heap->n;
+        heap->n = num->n;
+        num->n = temp;
+        reorder(num);
+    }
+}
+
+
+/**
  * sizeOfHeap - returns size of heap
  * @h: pointer to heap node
  * Return: size of heap
@@ -21,13 +49,31 @@ int sizeOfHeap(heap_t *h)
 }
 
 /**
+ * findLog - log of value to reposition heap
+ * @n: value
+ * Return: log o f the value n
+ */
+int findLog(int num)
+{
+    int n = 0;
+
+    while (num != 1)
+    {
+        num >>= 1;
+        n++;
+    }
+    return (n);
+}
+
+/**
  * heap_extract - removes the root node
  * @root: pointer to root of heap
  * Return: value in root node or else 0
  */
 int heap_extract(heap_t **root)
 {
-    int postn, value;
+    int postn, bitPostn, value, num;
+    heap_t **pos;
 
     if (!root)
         return (0);
@@ -35,6 +81,29 @@ int heap_extract(heap_t **root)
 
     if (postn == 0)
         value = 0;
-
+    else if (postn == 1)
+    {
+        value = (*root)->n;
+        *root = NULL;
+    }
+    else
+    {
+        value = (*root)->n;
+        bitPostn = findLog(postn);
+        num = 1 << (bitPostn - 1);
+        pos = root;
+        while (num)
+        {
+            if (num & postn)
+                pos = &((*pos)->right);
+            else
+                pos = &((*pos)->left);
+            num >>= 1;
+        }
+        (*root)->n = (*pos)->n;
+        free(*pos);
+        *pos = NULL;
+        reorder(*root);
+    }
     return (value);
 }
--- 0x0D-heap_extract/binary_trees.h
+++ 0x0D-heap_extract/binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stddef.h>
 #include <stdlib.h>
