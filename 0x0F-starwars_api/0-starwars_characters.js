18325e8251cc539a64dc589abb2fed4f9fc5449e
meghamohan <meghamohan1989@gmail.com>
2018-09-24 05:45:49 +0000

    first commit

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+const movie = process.argv.slice(2)[0];
+movieCharacters('https://swapi.co/api/people/', movie);
+
+function movieCharacters (url, movieId) {
+  request(url, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      const resp = JSON.parse(response.body);
+      url = resp.next; //response in the next page
+      let characters = resp.results;
+      characters.forEach(function (character) {
+        const movies = character.films;
+        movies.forEach(function (mov) {
+          if (mov.includes(movieId)) {
+            console.log(character.name);
+          }
+        });
+      });
+    }
+    if (url) { //print only if page isnt null
+      movieCharacters(url, movieId);
+    }
+  });
+}
---
+++ 0x0F-starwars_api/README.md
@@ -0,0 +1,4 @@
+##task 1
+- Write a script that prints all characters of a Star Wars movie
+- The first positional argument passed is the Movie ID - example: 3 = "Return of the Jedi"
+- Display one character name per line in the same order as the "characters" list in the film endpoint.
